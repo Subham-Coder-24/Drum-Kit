bdfeca486776c150d11d01ac342707423c8592a0
subham <subhamswaruppradhan2580@gmail.com>
2021-11-15 19:01:22 +0530

    Drum-kit-website

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+}
+
+function handleClick() {
+    var buttonElement = this.innerHTML;
+
+    makeSound(buttonElement);
+    buttonAnimation(buttonElement);
+}
+
+document.addEventListener("keypress", function (event) {  //event catch that key and passes by function and return all a arry which have all the property of that key
+
+    makeSound(event.key);                                  // event.key return that pertipular key
+
+    buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        default:
+            break;
+
+    }
+}
+
+
+function buttonAnimation(currentKey) {
+    var raja = document.querySelector("."+currentKey)
+    
+    raja.classList.add("pressed");
+
+    setTimeout(function () 
+    {   raja.classList.remove("pressed");  } ,100     );
+    
+}
+
+
+
+
+
+
+
+
+
+
